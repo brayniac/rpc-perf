58046efc34886f19a4992b4118ae99a39ccbc70f
Brian Martin <brayniac@gmail.com>
2023-05-30 12:59:06 -0700

    debugging

--- src/clients/redis2/commands/set.rs
+++ src/clients/redis2/commands/set.rs
@@ -28,6 +28,7 @@ pub fn validate_response(response: Response) -> std::result::Result<(), ()> {
             Ok(())
         }
         _ => {
+            println!("set ex");
             SET_EX.increment();
             Err(())
         }
